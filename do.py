2f42a7d475780b453cc2a300e6f424e335ca28c9
Dennis Priskorn <egils@riseup.net>
2021-05-29 10:11:08 +0200

    Added comment so people see how this was generated

--- do.py
+++ do.py
@@ -20,6 +20,7 @@ from datetime import datetime
 with open('items.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
+    print("<!-- generated using https://github.com/dpriskorn/wikidata2wikivoyage -->")
     for row in csv_reader:
         if line_count == 0:
             # print(f'Column names are {", ".join(row)}')
